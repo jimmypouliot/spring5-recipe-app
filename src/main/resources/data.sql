56e420555770b08c4162e0bd40ab41a3844f5b2f
Jimmy Pouliot <jimmypouliot@gmail.com>
2019-02-17 14:24:18 -0500

    Setup Category <--> Recipe relation, repositories and data init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert INTO CATEGORY (description) values ('American');
+insert INTO CATEGORY (description) values ('Italian');
+insert INTO CATEGORY (description) values ('Mexican');
+insert INTO CATEGORY (description) values ('Fast Food');
+insert into unit_of_measure (description) values ('Teaspoon');
+insert into unit_of_measure (description) values ('Tablespoon');
+insert into unit_of_measure (description) values ('Cup');
+insert into unit_of_measure (description) values ('Pinch');
+insert into unit_of_measure (description) values ('Ounce');
